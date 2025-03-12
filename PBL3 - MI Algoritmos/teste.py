f127dfde609c0045e3c022d0e922f3c9c7fbc631
gimenes. <gimeness.lopes@outlook.com>
2025-03-12 14:44:06 -0300

    Delete PBL3 - MI Algoritmos/teste.py

--- PBL3 - MI Algoritmos/teste.py
+++
@@ -1,8 +0,0 @@
-dic = {'Maria': '17 anos', 'João': '15 anos', 'Marcelo': '20 anos'}
-
-nome_procurado = input("Qual o nome a procurar? ")
-
-if not nome_procurado in dic:
-    print ('Este nome NÃO está no dicionário')
-else:
-    print ('Este nome está no dicionário')
